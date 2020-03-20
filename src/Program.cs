a89c029f4d3f4fd7f7a8d7d809eccd8e21f3600a
Brad Parker <crustydog9@gmail.com>
2020-03-20 19:43:56 +1100

    Initial file uploads

---
+++ src/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InterestCalc
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Are you looking to find out interest or time?");
+            Console.WriteLine("1. Interest");
+            Console.WriteLine("2. Time");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            if (number == 1)
+            {
+                Interest();
+            }
+            else if (number == 2)
+            {
+                Time();
+            }
+            else
+            {
+                Console.WriteLine("Please input a valid number");
+                Console.ReadLine();
+                Main();
+            }
+        }
+
+        static void Interest()
+        {
+            
+        }
+
+        static void Time()
+        {
+
+        }
+    }
+}
